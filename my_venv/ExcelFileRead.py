e894ca59e5802599b0dec175bd854c49671894b4
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-29 00:10:15 +0600

    excel file read

---
+++ my_venv/ExcelFileRead.py
@@ -0,0 +1,11 @@
+# importing pandas
+import pandas as pd
+
+# importing excel file
+df = pd.read_csv('Daffodil_Cource_List.csv')
+
+print(df)
+print("updated")
+print(df.head())
+print("updated")
+print(df.tail())
